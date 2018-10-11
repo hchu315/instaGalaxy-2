b0f985450ea5bf3191f88f0f59f480a10bd0bb32
Henry Chu <chu.henry@hotmail.com>
2018-10-10 21:54:13 -0700

    start reducers for AUTH

--- InstaGalaxy/test/controllers/static_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class StaticControllerTest < ActionDispatch::IntegrationTest
-  test "should get pages" do
-    get static_pages_url
-    assert_response :success
-  end
-
-end
