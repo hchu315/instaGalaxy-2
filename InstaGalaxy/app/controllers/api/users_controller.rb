e19ce8addc191b0fbceb430173b6cb817e7adfc4
Henry Chu <chu.henry@hotmail.com>
2018-10-10 17:24:55 -0700

    Move project to root directory

---
+++ InstaGalaxy/app/controllers/api/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    if @user
+      render "api/users/show"
+      # FIX THIS
+    else
+      flash.now[:errors] = user.errors.full_messages
+    end
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      login(@user)
+      render "api/users/show"
+      # FIX THIS??
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :email, :full_name, :bio)
+  end
+
+end
---
+++ InstaGalaxy/app/models/comment.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint(8)        not null, primary key
+#  user_id    :integer          not null
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Comment < ApplicationRecord
+end
