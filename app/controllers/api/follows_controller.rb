665c89ea087cb86de856e76b6dc2621f425bcd21
Henry Chu <chu.henry@hotmail.com>
2019-06-18 21:16:08 -0700

    implement follows controller

---
+++ app/controllers/api/follows_controller.rb
@@ -0,0 +1,17 @@
+class Api::FollowsController < ApplicationController
+  def create
+    @follow = Follow.new(follow_params)
+    @follow.user_id = current_user.id
+
+    if @follow.save
+      
+    end
+  end
+
+  def destroy
+  end
+
+  def follow_params
+    params.require(:follow).permit(:follower_id)
+  end
+end
