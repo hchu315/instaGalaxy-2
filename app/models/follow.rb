8137fe43570dd5b000f5b87ac15efbceb5e96d18
Henry Chu <chu.henry@hotmail.com>
2019-07-01 11:40:31 -0700

    fix syntax error on follow model

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -13,10 +13,10 @@ class Follow < ApplicationRecord
   validates :user_id, :follower_id, presence: true
 
   belongs_to :followee,
-  foreign_key :user_id,
-  class_name :User
+  foreign_key: :user_id,
+  class_name: :User
 
   belongs_to :follower,
-  foreign_key :follower_id,
-  class_name :User
+  foreign_key: :follower_id,
+  class_name: :User
 end
