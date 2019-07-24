1eb523b880b4a60199004503470850b19ab3064f
Henry Chu <chu.henry@hotmail.com>
2019-07-24 10:36:05 -0700

    uncomment follow controller and model

--- app/controllers/api/follows_controller.rb
+++ app/controllers/api/follows_controller.rb
@@ -1,18 +1,18 @@
 class Api::FollowsController < ApplicationController
-  def create
-    @follow = Follow.new(follow_params)
-    @follow.user_id = current_user.id
+  # def create
+  #   @follow = Follow.new(follow_params)
+  #   @follow.user_id = current_user.id
 
-    if @follow.save
-      render "api/follows"
-    end
-  end
+  #   if @follow.save
+  #     render "api/follows"
+  #   end
+  # end
 
-  def destroy
-    # @follow = 
-  end
+  # def destroy
+  #   # @follow = 
+  # end
 
-  def follow_params
-    params.require(:follow).permit(:follower_id)
-  end
+  # def follow_params
+  #   params.require(:follow).permit(:follower_id)
+  # end
 end
--- app/models/follow.rb
+++ app/models/follow.rb
@@ -10,13 +10,13 @@
 #
 
 class Follow < ApplicationRecord
-  validates :user_id, :follower_id, presence: true
+  # validates :user_id, :follower_id, presence: true
 
-  belongs_to :followee,
-  foreign_key: :user_id,
-  class_name: :User
+  # belongs_to :followee,
+  # foreign_key: :user_id,
+  # class_name: :User
 
-  belongs_to :follower,
-  foreign_key: :follower_id,
-  class_name: :User
+  # belongs_to :follower,
+  # foreign_key: :follower_id,
+  # class_name: :User
 end
